0b73bf17235ca7dd39012597022c77f67799e4be
Brian Meeker <brian.meeker@getthru.io>
2026-06-30 14:00:57 -0400

    Wire up the search UI with sort_input

--- lib/tunez_web/live/artists/index_live.ex
+++ lib/tunez_web/live/artists/index_live.ex
@@ -10,11 +10,13 @@ defmodule TunezWeb.Artists.IndexLive do
   end
 
   def handle_params(params, _url, socket) do
+    sort_by = Map.get(params, "sort_by") |> validate_sort_by()
     query_text = Map.get(params, "q", "")
-    artists = Tunez.Music.search_artists!(query_text)
+    artists = Tunez.Music.search_artists!(query_text, query: [sort_input: sort_by])
 
     socket =
       socket
+      |> assign(:sort_by, sort_by)
       |> assign(:query_text, query_text)
       |> assign(:artists, artists)
 
@@ -26,6 +28,7 @@ defmodule TunezWeb.Artists.IndexLive do
     <Layouts.app {assigns}>
       <.header responsive={false}>
         <.h1>Artists</.h1>
+        <:action><.sort_changer selected={@sort_by} /></:action>
         <:action>
           <.search_box query={@query_text} method="get" data-role="artist-search" phx-submit="search" />
         </:action>
@@ -152,8 +155,8 @@ defmodule TunezWeb.Artists.IndexLive do
 
   defp sort_options do
     [
-      {"recently updated", "updated_at"},
-      {"recently added", "inserted_at"},
+      {"recently updated", "-updated_at"},
+      {"recently added", "-inserted_at"},
       {"name", "name"}
     ]
   end
@@ -177,8 +180,8 @@ defmodule TunezWeb.Artists.IndexLive do
     {:noreply, push_patch(socket, to: ~p"/?#{params}")}
   end
 
-  def handle_event("search", %{"query" => query}, socket) do
-    params = remove_empty(%{q: query})
+  def handle_event("search", %{"query" => query, "sort_by" => sort_by}, socket) do
+    params = remove_empty(%{q: query, sort_by: sort_by})
     {:noreply, push_patch(socket, to: ~p"/?#{params}")}
   end
 
