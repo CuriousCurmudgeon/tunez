8274e4ef23e4ebc40c35bf321ef03842eca84160
Brian Meeker <brian.meeker@getthru.io>
2026-06-30 13:04:33 -0400

    Keep track of artist's previous names

--- lib/tunez/music/artist.ex
+++ lib/tunez/music/artist.ex
@@ -7,8 +7,27 @@ defmodule Tunez.Music.Artist do
   end
 
   actions do
-    defaults [:create, :read, :update, :destroy]
+    defaults [:create, :read, :destroy]
     default_accept [:name, :biography]
+
+    update :update do
+      require_atomic? false
+      accept [:name, :biography]
+
+      change fn changeset, _context ->
+               new_name = Ash.Changeset.get_attribute(changeset, :name)
+               previous_name = Ash.Changeset.get_data(changeset, :name)
+               previous_names = Ash.Changeset.get_data(changeset, :previous_names)
+
+               names =
+                 [previous_name | previous_names]
+                 |> Enum.uniq()
+                 |> Enum.reject(fn name -> name == new_name end)
+
+               Ash.Changeset.change_attribute(changeset, :previous_names, names)
+             end,
+             where: [changing(:name)]
+    end
   end
 
   attributes do
@@ -16,6 +35,10 @@ defmodule Tunez.Music.Artist do
 
     attribute :name, :string do
       allow_nil? false
+    end
+
+    attribute :previous_names, {:array, :string} do
+      default []
     end
 
     attribute :biography, :string
