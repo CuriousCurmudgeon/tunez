0a4203ae10679ec06daf99fc780633f705f91232
Brian Meeker <brian.meeker@getthru.io>
2026-08-09 18:12:52 -0400

    Wire up GraphQL queries and mutations for artists

--- lib/tunez_web/graphql_schema.ex
+++ lib/tunez_web/graphql_schema.ex
@@ -4,19 +4,10 @@ defmodule TunezWeb.GraphqlSchema do
   use AshGraphql,
     domains: [Tunez.Music]
 
-  import_types Absinthe.Plug.Types
+  import_types(Absinthe.Plug.Types)
 
   query do
     # Custom Absinthe queries can be placed here
-    @desc """
-    Hello! This is a sample query to verify that AshGraphql has been set up correctly.
-    Remove me once you have a query of your own!
-    """
-    field :say_hello, :string do
-      resolve fn _, _, _ ->
-        {:ok, "Hello from AshGraphql!"}
-      end
-    end
   end
 
   mutation do
